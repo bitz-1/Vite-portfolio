c282a401d595ebfb5196e1115e4d010945d26a38
Bitz <apurv.mod007@gmail.com>
2024-01-29 19:52:57 +0530

    5 commit hero section added adjustment

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,12 +2,26 @@ import "../../scss/hero.scss"
 const Hero = () => {
   return (
     <div className="hero">
-     <div className="imageContainer">\
+      <div className="wrapper">
+        <div className="textContainer">
         <h2>APURV NANDGAONKAR</h2>
-        <h1>Software Engineer | Full-Stack Web developer \-</h1>
-        <img src="./hero.png" alt=""/>
-
+        <h1>Software Engineer | Full-Stack Web Developer \-</h1>
+        <div className="buttons">
+          <button>See the Latest Work</button>
+          <button>Contact Me</button>
+          <div>
+            <img src="/scroll.png"/>
+          </div>
+        </div>
+        <div className="slidingTextContainer">
+          Creative Planner Problem-Solver Collaborative Responsive 
+        </div>
+          
+        </div>
+     <div className="imageContainer">\
+      <img src="./hero.png" alt=""/>
      </div>
+    </div>
     </div>
   )
 }
