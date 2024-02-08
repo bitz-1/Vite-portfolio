f960f1ca9b7265db51364786812b288e18456612
Bitz <apurv.mod007@gmail.com>
2024-02-09 02:35:17 +0530

    9 commit build

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,5 +1,5 @@
 
-import "../../scss/Parallax.scss"
+import "../../scss/parallax.scss"
 import {motion , useScroll,useTransform} from "framer-motion";
 import {useRef} from "react";
 const Parallax = ({type}) => {
